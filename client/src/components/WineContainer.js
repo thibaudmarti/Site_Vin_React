939cfa8c723e4aaf4b7a9b123f9be32174fcefb8
Thibaud <thibaud.marti@orange.fr>
2022-04-15 14:59:08 +0200

    Add WineContainer

---
+++ client/src/components/WineContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WineContainer = () => {
+  return (
+    <div>
+      <h1>Wine container</h1>
+    </div>
+  );
+};
+
+export default WineContainer;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,9 +1,11 @@
 import React from "react";
+import WineContainer from "../components/WineContainer";
 import WineForm from "../components/WineForm";
 
 const Admin = () => {
   return (
     <>
+      <WineContainer />
       <WineForm />
     </>
   );
